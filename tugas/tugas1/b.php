26f9108e26b84f2dfd95b95ac3d0abe4bb7a0228
Izra29 <aziznauval042@gmail.com>
2024-02-20 12:48:49 +0700

    tugas1

---
+++ tugas/tugas1/b.php
@@ -0,0 +1,18 @@
+<?php
+$angka = "9";
+
+echo "Aku adalah angka <b>$angka</b><br>";
+
+$angka *= "5";
+echo "jika aku dikali 5, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka /= "2";
+echo "jika aku dibagi 2, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka += "75";
+echo "jika aku ditambah 75, maka aku sekarang menjadi <b>$angka</b><br>";
+
+$angka -= "20";
+echo "jika aku dikurang 20, maka aku sekarang menjadi <b>$angka</b><br>";
+
+?>
